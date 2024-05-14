bc843e1de97f3bdd0a2ade53ec9f1f563b9591cc
robertcost <80799591+robertcost@users.noreply.github.com>
2024-05-14 13:23:56 -0400

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,2 +1,16 @@
-connect-mggraph 
-get-mguser -All
+param (
+	[parameter(Mandatory = $true)]
+	[string]$user
+)
+
+    connect-mggraph
+    #Enable user
+    $userad = Get-MgUser -Filter "Mail eq '$user'"
+    Get-MgUser -Filter "Mail eq '$user'"
+    Write-Host $userad.Mail -ForegroundColor Green
+    #Set-AzureADUser -ObjectId $user -AccountEnabled $false
+    Write-host "ACCOUNT ENABLED?"
+    Write-host $userad.AccountEnabled
+    write-host " "
+    write-host " "
+    
